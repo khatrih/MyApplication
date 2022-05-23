8da0d803aab7a27cdfcf0fad8c25458d69ab278e
Hitesh Khatri <hitesh.khatri@matrixhive.com>
2022-05-23 16:50:21 +0530

    unnecessary code

--- app/src/main/java/com/example/myapplication/hiddenDetails.java
+++ app/src/main/java/com/example/myapplication/hiddenDetails.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 public class hiddenDetails extends BaseActivity{
-    private static final String countryName= "India";
 
     @Override
     public void getData() {
